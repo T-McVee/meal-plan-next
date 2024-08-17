da22c3a9d6a5319dfd984603861a255ef05b2072
Tim McVinish <iam@tmcvee.com>
2024-08-17 18:40:08 +1000

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import SideNav from "@/components/SideNav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={
+          "flex min-h-screen flex-col items-center justify-between p-24 bg-muted/40"
+        }
+      >
+        <SideNav />
+        {children}
+      </body>
     </html>
   );
 }
